00a30bb318331ab98a439bb591c2c548a93298ec
mutuki kurita <mu22026stuitand@gmail.com>
2026-09-24 14:20:02 +0900

    大まかな流れを追加

---
+++ jankennkazuate/janken.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//int Judge(int x,int y)
+//{
+//
+//}
+//int preparation()
+//{
+//
+//}
+void janken()
+{
+	
+}
---
+++ jankennkazuate/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+enum Mode
+{
+	MO_END_PROGRAM,//プログラムを終わる
+	MO_JANKEN,	//じゃんけん
+	MO_KAZUATE	//数当て
+};
+
+void SelectMode()
+{
+	int mode = 0;
+	cout << "モードを選択" << endl << "1.じゃんけん" << endl << "2.数当てゲーム" << endl << "0.終了" << endl << ">";
+	cin >> mode;
+
+	switch (mode)
+	{
+	case 0:
+		cout << "プログラムを終了します" << endl;
+		break;
+	case 1:
+		cout << "じゃんけんを開始" << endl;
+		break;
+	case 2:
+		cout << "数当てを開始" << endl;
+		break;
+	}
+
+}
+
+int main()
+{
+	SelectMode();
+
+}
